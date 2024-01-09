908ccd5275f2e349e08dce7468715311348bcc29
V1n1c1us-K-S-W4t4n4b3 <vinikswatanabe@outlook.com>
2024-01-09 09:25:40 -0400

    first commit

---
+++ app/src/main/java/com/kzdev/appnotification/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.kzdev.appnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kzdev.appnotification.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        setContentView(R.layout.activity_main)
+    }
+}
